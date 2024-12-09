420fda7b2108dfaa6e8bf879cf69748deff6b392
JinriBear <hennurunge@gmail.com>
2024-12-09 15:59:08 +0900

    feat: 메시지 수신 및 재송신 기능 구현
    - 클라이언트로부터 메시지를 수신
    - 수신한 메시지를 다시 클라이언트로 전송(접속 메시지, 채팅 메시지)

--- server/index.js
+++ server/index.js
@@ -1,9 +1,46 @@
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 8080 });
 
+const handleMessage = (msg) => {
+  const type = msg.type;
+  const nickname = msg.nickname;
+  const content = msg.content;
+  
+  const handleType = {
+    connect: JSON.stringify({
+      type: type,
+      content: `${nickname}님께서 접속하셨습니다.`,
+    }),
+    close: JSON.stringify({
+      type: type,
+      content: `${nickname}님께서 접속을 종료하였습니다.`,
+    }),
+    chat: JSON.stringify({
+      type: type,
+      nickname: nickname,
+      content: content,
+    }),
+  };
+
+  return handleType[type];
+}
+
+const broadcaseMessage = (server, message) => {
+  server.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN)
+      client.send(message);
+    });
+};
+
 wss.on('connection', (ws) => {
-  console.log("연결");
+  
+  ws.on('message', (msg) => {
+    const jsonParse = JSON.parse(msg.toString());
+    console.log(jsonParse);
+    const message = handleMessage(jsonParse);
+    broadcaseMessage(wss, message);
+  })
+
 
   ws.on('close', () => {
     console.log("연결 종료");
