029808a102112832e96c8386fa7a0e3832899c49
JinriBear <hennurunge@gmail.com>
2024-12-19 02:12:04 +0900

    feat: 접속종료 메시지 전송 기능 추가
     - userList를 추가하여 사용자 목록을 관리
     - 사용자가 접속 종료 시 나머지 사용자에게 접속종료 메시지 전송 기능 구현

--- server/index.js
+++ server/index.js
@@ -1,48 +1,59 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
-const handleMessage = (msg) => {
+const userList = new Map();
+
+const handleMessage = (msg, ws) => {
   const type = msg.type;
   const nickname = msg.nickname;
   const content = msg.content;
-  
-  const handleType = {
-    connect: JSON.stringify({
-      type: type,
-      content: `${nickname}님께서 접속하셨습니다.`,
-    }),
-    close: JSON.stringify({
-      type: type,
-      content: `${nickname}님께서 접속을 종료하였습니다.`,
-    }),
-    chat: JSON.stringify({
-      type: type,
-      nickname: nickname,
-      content: content,
-    }),
-  };
 
-  return handleType[type];
+  const handleType = {
+    connect: () => {
+      userList.set(ws, nickname);
+      return {
+        type: "connect",
+        content: `${nickname}님께서 접속하셨습니다.`,
+      };
+    },
+    close: () => {
+      userList.delete(ws);
+      return {
+        type: "close",
+        content: `${nickname}님께서 접속을 종료하였습니다.`,
+      }
+    },
+    chat: () => {
+      return {
+        type: "chat",
+        nickname: nickname,
+        content: content,
+      }
+    },
+  }
+  return handleType[type]();
 }
 
 const broadcaseMessage = (server, message) => {
   server.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN)
-      client.send(message);
+      client.send(JSON.stringify(message));
     });
 };
 
 wss.on('connection', (ws) => {
-  
+
   ws.on('message', (msg) => {
     const jsonParse = JSON.parse(msg.toString());
-    console.log(jsonParse);
-    const message = handleMessage(jsonParse);
-    broadcaseMessage(wss, message);
+    const message = handleMessage(jsonParse, ws);
+    broadcaseMessage(wss, message)
   })
 
-
   ws.on('close', () => {
-    console.log("연결 종료");
+    const message = handleMessage({
+      type: "close",
+      nickname: userList.get(ws),
+    }, ws);
+    broadcaseMessage(wss, message);
   })
 });
