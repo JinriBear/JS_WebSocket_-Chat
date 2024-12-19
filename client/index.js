18f829163a4842eef371c2add09598dce867f820
JinriBear <hennurunge@gmail.com>
2024-12-20 02:42:08 +0900

    feat: 닉네임 설정 기능 추가
     - 닉네임 설정을 위한 모달창 UI 구현
     - 서버접속 메시지와 종료 메시지 bold처리

--- client/index.js
+++ client/index.js
@@ -1,13 +1,16 @@
-const socket = new WebSocket("ws://localhost:8080");
 const chatLog = document.querySelector('#chat-log');
 const chatForm = document.querySelector('#chat-input-container');
 const chatInputBox = document.querySelector("#chat-input-box");
+const modal = document.querySelector("#modal-bg")
+const modalInput = document.querySelector('#nickname');
+const modalBtn = document.querySelector('#set-Nickname');
+let socket;
 
-const clientInfo = {
-  type: "connect",
-  nickname: "윈도우",
-  content: "",
-};
+const ClientInfo = function(nickname) {
+  this.type = "connect";
+  this.nickname = nickname;
+  this.content = "";
+}
 
 const handleMessage = (msg) => {
   const type = msg.type;
@@ -22,29 +25,50 @@ const handleMessage = (msg) => {
     chatLog.append(div);
   }  
   if(type === "connect" || type === "close"){
-    const message = document.createElement("p");
+    const message = document.createElement("b");
     message.innerText = content;
     div.append(message);
     chatLog.append(div);
   }
+
+  // 메시지를 받으면 채팅내역 스크롤 제일 아래로 내림
+  chatLog.scrollTop = chatLog.scrollHeight;
 }
 
-socket.addEventListener('open', () => {
-  socket.send(JSON.stringify(clientInfo));
-});
+modalBtn.addEventListener('click', function() {
+  const nickname = modalInput.value;
 
-socket.addEventListener('message', (event) => {
-  const jsonParse = JSON.parse(event.data);
-  handleMessage(jsonParse);
-})
-
-chatForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if(chatInputBox === "")
+  if(nickname === ""){
+    alert("닉네임을 입력해주세요");
     return;
-  clientInfo.type = "chat"
-  clientInfo.content = chatInputBox.value;
-  socket.send(JSON.stringify(clientInfo));
+  }
 
-  chatInputBox.value = "";
+  const clientInfo = new ClientInfo(nickname);
+  modal.style.display = "none";
+  socket = new WebSocket("ws://localhost:8080");
+
+  socket.addEventListener('open', () => {
+    socket.send(JSON.stringify(clientInfo));
+  });
+
+  socket.addEventListener('message', (event) => {
+    const jsonParse = JSON.parse(event.data);
+    handleMessage(jsonParse);
+  })
+  
+  chatForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if(chatInputBox === "")
+      return;
+    clientInfo.type = "chat"
+    clientInfo.content = chatInputBox.value;
+    socket.send(JSON.stringify(clientInfo));
+  
+    chatInputBox.value = "";
+  })
 })
+
+
+
+
+
